27efc4333a08076f672d5b337c584f1d18a03537
光光 <kevinliu830829@163.com>
2017-08-23 11:40:09 +0800

    修改调整读取文件

--- RoadWriteFile.py
+++ RoadWriteFile.py
@@ -3,11 +3,39 @@
 #VERSION:1.0
 #__OUTHOR__:guangguang
 #Email:kevinliu830829@163.com
-def ReadFile():
-    pass
+import xlrd,re
+class ReadFile():
+    def __index__(self,Filename):
+        self.Filename=Filename
+        self.DomailList=[]
+        pass
+    def ReturnDate(self):
+        FileType=self.Filename.stlit('.')[-1]
+        if FileType=='txt':
+            return self.ReadTxt()
+        elif FileType=='xls' or FileType=='xlsx':
+            return self.ReadExecL()
+    def ReadTxt(self):
+        data = open(self.Filename, 'r')
+        for i in data.readlines():
+            self.DomailList.append(i)
+        return self.DomailList
+        pass
+    def ReadExecL(self):
+        data = xlrd.open_workbook(self.Filename)
+        table = data.sheet_by_index(0)
+        nrows = table.nrows
+        for i in range(nrows):
+            self.DomailList.append(i)
+        return self.DomailList
+        pass
 def WriteFile():
     pass
-
+def CheckDomain(DomainList):
+    return
+    pass
 #DEBUG
 if __name__ == "__main__":
-    pass
+    DomainList=ReadFile('jiangsu.txt')
+    WriteDate=CheckDomain(DomainList)
+    WriteFile(WriteDate)
