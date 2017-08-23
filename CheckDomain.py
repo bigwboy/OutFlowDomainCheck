4ee30fb246bc86cc8b1192a577626c0a257ab1f4
光光 <kevinliu830829@163.com>
2017-08-23 10:14:06 +0800

    初始化小工具

---
+++ CheckDomain.py
@@ -0,0 +1,13 @@
+# -*- coding: utf8 -*-
+#time:2017/8/23 10:06
+#VERSION:1.0
+#__OUTHOR__:guangguang
+#Email:kevinliu830829@163.com
+import RoadWriteFile
+
+
+
+
+#DEBUG
+if __name__ == "__main__":
+    pass
---
+++ RoadWriteFile.py
@@ -0,0 +1,13 @@
+# -*- coding: utf8 -*-
+#time:2017/8/23 10:07
+#VERSION:1.0
+#__OUTHOR__:guangguang
+#Email:kevinliu830829@163.com
+def ReadFile():
+    pass
+def WriteFile():
+    pass
+
+#DEBUG
+if __name__ == "__main__":
+    pass
